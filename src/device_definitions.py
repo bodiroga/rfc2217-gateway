200084f2ee6e25f42e532c12e1adbd32d1cdfbaa
Aitor Iturrioz <riturrioz@gmail.com>
2020-06-02 20:29:59 +0200

    Update device definitions
    Signed-off-by: Aitor Iturrioz <riturrioz@gmail.com>

--- src/device_definitions.py
+++ src/device_definitions.py
@@ -1,3 +1,5 @@
-devices = [{"ID_MODEL": "0200", "ID_VENDOR": "0658", "NAME": "Aeotec ZStick Gen5", "CATEGORY": "zwave", "PORT": 5555},
-           {"ID_MODEL": "ea60", "ID_VENDOR": "10c4", "NAME": "Aeotec ZStick Gen2", "CATEGORY": "zwave", "PORT": 5556},
-           {"ID_MODEL": "6001", "ID_VENDOR": "0403", "NAME": "Plugwise Stick", "CATEGORY": "plugwise", "PORT": 5557}]
+devices = [{"ID": {"ID_MODEL_ID": "0200", "ID_VENDOR_ID": "0658", "ID_VENDOR_ENC": "0658"}, "NAME": "Aeotec ZStick Gen5", "PORT": 5555},
+           {"ID": {"ID_MODEL_ID": "ea60", "ID_VENDOR_ID": "10c4", "ID_VENDOR_ENC": "Silicon Labs"}, "NAME": "Aeotec ZStick Gen2", "PORT": 5556},
+           {"ID": {"ID_MODEL_ID": "6001", "ID_VENDOR_ID": "0403", "ID_VENDOR_ENC": ""}, "NAME": "Plugwise Stick", "PORT": 5557},
+           {"ID": {"ID_MODEL_ID": "6001", "ID_VENDOR_ID": "0403", "ID_VENDOR_ENC": "RFXCOM"}, "NAME": "RFXCOM RFXtrx433", "PORT": 5558},
+           {"ID": {"ID_MODEL_ID": "6001", "ID_VENDOR_ID": "0403", "ID_VENDOR_ENC": "EnOcean GmbH"}, "NAME": "Enocean", "PORT": 5559}]
