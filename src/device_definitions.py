e318bf2669ac85c5505fd97667348981c349d3cf
Aitor Iturrioz <riturrioz@gmail.com>
2020-06-03 18:23:16 +0200

    Remove Plugwise stick and update Enocean syntax
    Signed-off-by: Aitor Iturrioz <riturrioz@gmail.com>

--- src/device_definitions.py
+++ src/device_definitions.py
@@ -1,5 +1,4 @@
 devices = [{"ID": {"ID_MODEL_ID": "0200", "ID_VENDOR_ID": "0658", "ID_VENDOR_ENC": "0658"}, "NAME": "Aeotec ZStick Gen5", "PORT": 5555},
            {"ID": {"ID_MODEL_ID": "ea60", "ID_VENDOR_ID": "10c4", "ID_VENDOR_ENC": "Silicon Labs"}, "NAME": "Aeotec ZStick Gen2", "PORT": 5556},
-           {"ID": {"ID_MODEL_ID": "6001", "ID_VENDOR_ID": "0403", "ID_VENDOR_ENC": ""}, "NAME": "Plugwise Stick", "PORT": 5557},
-           {"ID": {"ID_MODEL_ID": "6001", "ID_VENDOR_ID": "0403", "ID_VENDOR_ENC": "RFXCOM"}, "NAME": "RFXCOM RFXtrx433", "PORT": 5558},
-           {"ID": {"ID_MODEL_ID": "6001", "ID_VENDOR_ID": "0403", "ID_VENDOR_ENC": "EnOcean GmbH"}, "NAME": "Enocean", "PORT": 5559}]
+           {"ID": {"ID_MODEL_ID": "6001", "ID_VENDOR_ID": "0403", "ID_VENDOR_ENC": "RFXCOM"}, "NAME": "RFXCOM RFXtrx433", "PORT": 5557},
+           {"ID": {"ID_MODEL_ID": "6001", "ID_VENDOR_ID": "0403", "ID_VENDOR_ENC": "EnOcean\\x20GmbH"}, "NAME": "Enocean", "PORT": 5558}]
