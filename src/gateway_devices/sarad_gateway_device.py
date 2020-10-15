04082e653a761b4cdc0d3ce2cd666ebcf25681d0
rifoerster <riccardo.foerster@sarad.de>
2020-10-15 12:52:15 +0200

    Update sarad_gateway_device.py
    Fixed Sarad Gateway

--- src/gateway_devices/sarad_gateway_device.py
+++ src/gateway_devices/sarad_gateway_device.py
@@ -24,8 +24,9 @@ class SaradGatewayDevice(GenericGatewayDevice):
         try:
             self.__devi = self.__cluster.update_connected_instruments([self.get_serial_port()])
         except Exception:
-            logger.error("USB Device Access Failed")
+            logger.error(f"USB Device Access Failed {device}")
             pass
+        self.get_properties()
     
         
     def get_serial_id(self):
@@ -37,27 +38,27 @@ class SaradGatewayDevice(GenericGatewayDevice):
         
     def get_properties(self):
         if len(self.__devi)==1 :
-            model_id = self.device.get("ID_MODEL_ID", "")
-            model = self.device.get("ID_MODEL", "")
-            model_enc = self.device.get("ID_MODEL_ENC", "")
-            model_db = self.device.get("ID_MODEL_FROM_DATABASE", "")
-            vendor_id = self.device.get("ID_VENDOR_ID", "")
-            vendor = self.device.get("ID_VENDOR_FROM_DATABASE", "")
+            self.model_id = self.device.get("ID_MODEL_ID", "")
+            self.model = self.device.get("ID_MODEL", "")
+            self.model_enc = self.device.get("ID_MODEL_ENC", "")
+            self.model_db = self.device.get("ID_MODEL_FROM_DATABASE", "")
+            self.vendor_id = self.device.get("ID_VENDOR_ID", "")
+            self.vendor = self.device.get("ID_VENDOR_FROM_DATABASE", "")
             self.vendor_enc = self.device.get("ID_VENDOR_ENC", "")
-            vendor_db = self.device.get("ID_VENDOR_FROM_DATABASE", "")
-            serial_short = str(self.__devi[0].get_id())
-            self.serial = f'{vendor_enc}_{serial_short}'
+            self.vendor_db = self.device.get("ID_VENDOR_FROM_DATABASE", "")
+            self.serial_short = str(self.__devi[0].get_id())
+            self.serial = f'{self.vendor_enc}_{self.serial_short}'
 
-            properties = { "MODEL_ID": model_id, "MODEL": model,
-                    "MODEL_ENC": model_enc, "MODEL_DB": model_db,
-                    "VENDOR_ID": vendor_id, "VENDOR": vendor,
-                    "VENDOR_ENC": vendor_enc, "VENDOR_DB": vendor_db,
-                    "SERIAL": "{}_{}".format(model,serial_short), "SERIAL_SHORT": serial_short
+            properties = { "MODEL_ID": self.model_id, "MODEL": self.model,
+                    "MODEL_ENC": self.model_enc, "MODEL_DB": self.model_db,
+                    "VENDOR_ID": self.vendor_id, "VENDOR": self.vendor,
+                    "VENDOR_ENC": self.vendor_enc, "VENDOR_DB": self.vendor_db,
+                    "SERIAL": "{}_{}".format(self.model,self.serial_short), "SERIAL_SHORT": self.serial_short
                     }
             return properties
         else:
             return super().get_properties()
         
-    def get_name_unique(self):    
-        return f'{self.vendor_enc}_{self.serial}'
+    def get_name_unique(self): 
+        return f'{self.serial}'
 
